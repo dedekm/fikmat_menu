93ecdb4a3877a7c1dd93e32da54214b74fb88633
dedekm <dedekmm@gmail.com>
2021-09-17 19:02:25 +0200

    add config json files

--- src/Main.gd
+++ src/Main.gd
@@ -1,13 +1,36 @@
 extends Node2D
 
-const games = ["pong1.x86_64",
-               "pong2.x86_64"]
-
+var games = []
 var index := 0
 var current_game_pid: int
 
 func _ready() -> void:
-  print(OS.get_process_id())
+  var configs = []
+  var dir = Directory.new()
+  dir.open('res://configs')
+  dir.list_dir_begin()
+    
+  var filename = dir.get_next()
+  while filename != "":
+    if filename.ends_with(".json"):
+      configs.append(dir.get_current_dir() + "/" + filename)
+    filename = dir.get_next()
+
+  dir.list_dir_end()
+  
+  configs.sort()
+
+  for config in configs:
+    var file = File.new()
+    file.open(config, file.READ)
+    var json = file.get_as_text()
+    var json_data = JSON.parse(json).result
+    file.close()
+    
+    # TODO: validate config data
+    games.append(json_data)
+    print(json_data)
+  
   pass
 
 func _input(event: InputEvent) -> void:
@@ -15,13 +38,13 @@ func _input(event: InputEvent) -> void:
     match event.scancode:
       KEY_LEFT:
         _move_index(-1)
-        print(games[index])
+        print(games[index].name)
       KEY_RIGHT:
         _move_index(1)
-        print(games[index])
+        print(games[index].name)
       KEY_ENTER:
-        print("launching " + games[index])
-        current_game_pid = OS.execute("games/" + games[index], [], false)
+        print("launching " + games[index].name)
+        current_game_pid = OS.execute("games/" + games[index].filename, [], false)
         print("current game PID - " + str(current_game_pid))
         OS.execute("bin/idle_check", [current_game_pid, OS.get_process_id()], false)
 
