f2f9625e2f4ab1a4a1f16120a7ea5a3d05862311
dedekm <dedekmm@gmail.com>
2021-10-23 14:59:45 +0200

    add thumbs

--- src/Main.gd
+++ src/Main.gd
@@ -1,7 +1,8 @@
-extends Node2D
+extends Spatial
 
 var games = []
-var index := 0
+var thumbs = []
+var index: int
 var current_game_pid: int
 
 func _ready() -> void:
@@ -30,23 +31,51 @@ func _ready() -> void:
     # TODO: validate config data
     games.append(json_data)
     print(json_data)
+
+  index = floor(games.size() / 2)
   
-  pass
+  for i in games.size():
+    var thumb: = MeshInstance.new()
+    thumb.mesh = PlaneMesh.new()
+    
+    var texture := load("res://assets/" + games[i].thumb)
+    var material = SpatialMaterial.new()
+    material.albedo_texture = texture
+    thumb.mesh.surface_set_material(0, material)
+    thumb.rotation_degrees.x = 90
+    
+    add_child(thumb)
+    thumbs.append(thumb)
+    
+  _update_thumbs()
+
+func _update_thumbs() -> void:
+  for i in games.size():
+    var x = 2 + 0.5 * abs(i - index)
+    var thumb = thumbs[i]
+
+    if i < index:
+      thumb.translation.x = -1 * x
+      thumb.translation.z = -1
+      thumb.rotation_degrees.y = 75
+    elif i > index:
+      thumb.translation.x = x
+      thumb.translation.z = -1
+      thumb.rotation_degrees.y = -75
+    else:
+      thumb.translation.x = 0
+      thumb.translation.z = 0
+      thumb.rotation_degrees.y = 0
 
 func _input(event: InputEvent) -> void:
   if event is InputEventKey and event.pressed:
     match event.scancode:
       KEY_LEFT:
         _move_index(-1)
-        print(games[index].name)
+        _update_thumbs()
       KEY_RIGHT:
         _move_index(1)
-        print(games[index].name)
-      KEY_ENTER:
-        print("launching " + games[index].name)
-        current_game_pid = OS.execute("games/" + games[index].filename, [], false)
-        print("current game PID - " + str(current_game_pid))
-        OS.execute("bin/idle_check", [current_game_pid, OS.get_process_id()], false)
+        _update_thumbs()
 
 func _move_index(n: int) -> void:
   var i := index + n
@@ -56,3 +85,5 @@ func _move_index(n: int) -> void:
     index = games.size() - 1
   else:
     index = index + n
+
+  print(games[index].name)
