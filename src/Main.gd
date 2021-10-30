feb061a01b50f1badc893c13547e2dab40680bcf
dedekm <dedekmm@gmail.com>
2021-10-30 22:46:28 +0200

    add thumbnails animation

--- src/Main.gd
+++ src/Main.gd
@@ -2,15 +2,19 @@ extends Spatial
 
 var games = []
 var thumbs = []
-var index: int
-var current_game_pid: int
+var index : int
+var current_game_pid : int
+
+var tween_speed := 0.5
+
+onready var tween := get_node("Tween")
 
 func _ready() -> void:
   var configs = []
   var dir = Directory.new()
   dir.open('res://configs')
   dir.list_dir_begin()
-    
+
   var filename = dir.get_next()
   while filename != "":
     if filename.ends_with(".json"):
@@ -18,7 +22,7 @@ func _ready() -> void:
     filename = dir.get_next()
 
   dir.list_dir_end()
-  
+
   configs.sort()
 
   for config in configs:
@@ -27,27 +31,27 @@ func _ready() -> void:
     var json = file.get_as_text()
     var json_data = JSON.parse(json).result
     file.close()
-    
+
     # TODO: validate config data
     games.append(json_data)
     print(json_data)
 
   index = floor(games.size() / 2)
-  
+
   for i in games.size():
-    var thumb: = MeshInstance.new()
+    var thumb := MeshInstance.new()
     thumb.mesh = PlaneMesh.new()
-    
     var texture := load("res://assets/" + games[i].thumb)
     var material = SpatialMaterial.new()
     material.albedo_texture = texture
     thumb.mesh.surface_set_material(0, material)
     thumb.rotation_degrees.x = 90
-    
+
     add_child(thumb)
     thumbs.append(thumb)
-    
+
   _update_thumbs()
+
 
 func _update_thumbs() -> void:
   for i in games.size():
@@ -55,17 +59,26 @@ func _update_thumbs() -> void:
     var thumb = thumbs[i]
 
     if i < index:
-      thumb.translation.x = -1 * x
-      thumb.translation.z = -1
-      thumb.rotation_degrees.y = 75
+      _interpolate_thumb_translation(thumb, -1 * x, -1)
+      _interpolate_thumb_rotation(thumb, 75)
     elif i > index:
-      thumb.translation.x = x
-      thumb.translation.z = -1
-      thumb.rotation_degrees.y = -75
+      _interpolate_thumb_translation(thumb, x, -1)
+      _interpolate_thumb_rotation(thumb, -75)
     else:
-      thumb.translation.x = 0
-      thumb.translation.z = 0
-      thumb.rotation_degrees.y = 0
+      _interpolate_thumb_translation(thumb, 0, 0)
+      _interpolate_thumb_rotation(thumb, 0)
+
+      _update_game_description(games[i])
+
+  tween.start()
+
+func _interpolate_thumb_translation(thumb, x, z) -> void:
+  tween.interpolate_property(thumb, "translation", thumb.translation, Vector3(x, 0, z), tween_speed, Tween.TRANS_LINEAR, Tween.EASE_IN)
+
+
+func _interpolate_thumb_rotation(thumb, degrees) -> void:
+  tween.interpolate_property(thumb, "rotation_degrees", thumb.rotation_degrees, Vector3(90, degrees, 0), tween_speed, Tween.TRANS_LINEAR, Tween.EASE_IN)
+
 
 func _input(event: InputEvent) -> void:
   if event is InputEventKey and event.pressed:
